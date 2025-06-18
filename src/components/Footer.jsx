5b49328f1ae70344dda91010551adb1620f36b51
Ian Frank Odundo <78843288+ianfranko@users.noreply.github.com>
2025-06-18 11:18:08 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-6 text-center mt-auto">
       <div>
-        © {new Date().getFullYear()} Rwanda Convention Bureau. All rights reserved.
+        © {new Date().getFullYear()} Cutting Edge Consultants Ltd. All rights reserved.
       </div>
     </footer>
   );
